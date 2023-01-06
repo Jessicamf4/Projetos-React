42f5bad5339337ac068d8519baf11b521c2d50ee
Jessicamf4 <jessicamarquesfialho0@gmail.com>
2023-01-06 20:24:15 -0300

    Add 'Nova tarefa'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 
 import Navbar from "./components/Navbar/Navbar";
 import TaskLista from './components/TaskList/TaskList';
 
+const task = {
+  id:0,
+  title: "Nova Tarefa",
+  state: "pendente"
+}
+
+let idAcc = 0;
+const gerarId = () => {
+  idAcc = idAcc +1;
+  return idAcc;
+}
+
 export default function App(){
+
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask ={
+      id: gerarId(),
+      title,
+      state
+    };
+    setTasks((existingTasks)=>{
+      return [...existingTasks, newTask];
+    })
+  }
+
   return (
     <div className='App'>
       <Navbar />
       <div className='container'>
-        <TaskLista title="Pendente"/>
-        <TaskLista title="Fazendo" />
-        <TaskLista title="Completo"/>
+        <TaskLista title="Pendente" onAddTask={addTask} tasks={tasks}/>
       </div>
     </div>
   );
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import "./taskList.css";
-import { useState } from "react";
+
 import PropTypes from 'prop-types';
 
 
 
-export default function TaskLista({title}){
-    const [count, setCount] = useState(0);
+export default function TaskLista({title, onAddTask, tasks}){
 
-    const increment = () => {
-        setCount((currentCount)=>{
-            return currentCount + 1;
-        })
-    }
+    const addTask = () => {
+        onAddTask("Nova Tarefa", "Pendente");
+    };
 
     return(
         <div className='taskList'>
- 
             <div className='title'>{title}</div>
             <div className='content'>
-                {count}
-                <button onClick={increment}>Incrementar</button>
+                {tasks.map((task)=>{
+                    return <div key={task.id}>{task.title}</div>;
+                })}
             </div>
+            <button onClick={addTask}>Adicionar Tarefa</button>;
         </div>
     )
 }
 
 TaskLista.propTypes = {
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    onAddTask: PropTypes.func.isRequired,
+    tasks: PropTypes.array.isRequired
 }
 //props são imutáveis usadas para renderizar elementos
 //Definir o tipo das props e assim evitar erros de desenvolvimento
